7aee8c60f6d1e1d185b5eeb7b3a4595219e87cbe
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 10:56:03 +0530

    [Mihir] Added Condition to check for minimum working days/hours

--- Program.cs
+++ Program.cs
@@ -10,16 +10,17 @@ namespace EmployeeWageProblem
             const int IS_FULL_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
             const int NUM_OF_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_MONTH = 100;
 
             int empHrs = 0;
-            int empWage = 0;
-            int totalWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-
                 switch (empCheck)
                 {
                     case IS_PART_TIME:
@@ -32,10 +33,11 @@ namespace EmployeeWageProblem
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalWage += empWage;
+                totalEmpHrs += empHrs;
             }
-            Console.WriteLine("Total Monthly Employee Wage:" + totalWage);
+            Console.WriteLine("Days:" + totalWorkingDays + " Emp Hrs : " + totalEmpHrs);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage:" + totalEmpWage);
         }
     }
 }
