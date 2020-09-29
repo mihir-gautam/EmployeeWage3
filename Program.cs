de5f13c2b764cb5a542a2df066a78fa4af6b2ecc
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-29 09:27:02 +0530

    [Mihir] Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to the employee wage problem");
+        }
+    }
+}
