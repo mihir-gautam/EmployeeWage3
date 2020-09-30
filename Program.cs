46498520be9ba6ff0dc53be3247a203123b52464
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-09-30 15:36:10 +0530

    [Mihir] Refactor Computation of employee wage for multiple companies

--- Program.cs
+++ Program.cs
@@ -4,45 +4,10 @@ namespace EmployeeWageProblem
 {
     class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
-        public static int computeEmpWage()
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random2 = new Random();
-                int checkShift = random2.Next(0, 3);
-                switch (checkShift)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + " Emp Hrs : " + totalEmpHrs);
-            }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(String[] args)
         {
-            computeEmpWage();
+        ForMultipleCompanies.computeEmpWage("Microsoft", 20, 2, 10);
+        ForMultipleCompanies.computeEmpWage("Apple", 10, 4, 20);
         }
     }
 }
